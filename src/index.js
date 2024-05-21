12e32922444446ac1ce4323f270c4124ac59fc95
Narges Abbasi <nargesabbasi2976@gmail.com>
2024-05-21 08:19:17 +0330

    Adding search functionality using API call

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-function handleCityName(event) {
-  event.preventDefault();
-  let SearchedCityName = document.querySelector("#search-form-input");
+function refreshWeather(response) {
+  let elementTemperature = document.querySelector("#temperature");
+  let currentTemperature = Math.round(response.data.temperature.current);
   let cityName = document.querySelector("#city");
-  cityName.innerHTML = SearchedCityName.value;
+  cityName.innerHTML = response.data.city;
+  elementTemperature.innerHTML = currentTemperature;
+}
+
+function searchCity(city) {
+  let apikey = "b318adc2a4e42ff5a70at1cf59b30fbo";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearch(event) {
+  event.preventDefault();
+  let searchedCityName = document.querySelector("#search-form-input");
+  searchCity(searchedCityName.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleCityName);
+searchFormElement.addEventListener("submit", handleSearch);
+searchCity("Tehran");
